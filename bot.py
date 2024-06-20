26fbfeaead10dc82824c743b0b6143c851796686
no-0-name <gordei.gordey.2007@mail.ru>
2024-06-20 23:07:24 +0300

    add async openai client and delete function

--- bot.py
+++ bot.py
@@ -4,50 +4,27 @@ import asyncio
 import io
 import logging
 import types
-from dotenv import load_dotenv
-from openai import OpenAI
+import time
+from pydantic import Field
+from pydantic_settings import BaseSettings
+from openai import AsyncOpenAI
 from aiogram import Bot, Dispatcher, F, Router, types
 from aiogram.filters import CommandStart
 from pydub import AudioSegment
 
 
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
-OPENAI_API_KEY = os.getenv('OPENAIP_API_KEY')
+class Settings(BaseSettings):
+    TOKEN: str = Field(validation_alias="TOKEN")
+    OPENAI_API_KEY: str = Field(validation_alias="OPENAI_API_KEY")
 
-bot = Bot(token=TOKEN)
+
+settings = Settings()
+bot = Bot(token=settings.TOKEN)
 dp = Dispatcher()
 router = Router()
-client = OpenAI(
-    api_key=OPENAI_API_KEY
+client = AsyncOpenAI(
+    api_key=settings.OPENAI_API_KEY
 )
-
-
-def audio_to_text(audio_file_path: str) -> str:
-    with open(audio_file_path, 'rb') as audio_file:
-        transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file, response_format="text")
-    
-    text = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": transcript},
-        ]
-    )
-
-    speech_file_path = "voice_files/speech.mp3"
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice="alloy",
-        input=text.choices[0].message.content
-    )
-    
-    response.stream_to_file(speech_file_path)
-    speech_ogg_file = "voice_files/speech.ogg"
-    AudioSegment.from_file(speech_file_path, format="mp3").export(
-        speech_ogg_file, format="ogg"
-    )
-
-    return speech_file_path
 
 
 async def save_voice(bot: Bot, voice: types.Voice) -> str:
@@ -55,34 +32,87 @@ async def save_voice(bot: Bot, voice: types.Voice) -> str:
     voice_ogg = io.BytesIO()
     await bot.download_file(voice_file_info.file_path, voice_ogg)
 
-    voice_mp3_path = f"voice_files/voice.mp3"
+    voice_mp3_path = f"voice_files/voice-{voice.file_unique_id}.mp3"
     AudioSegment.from_file(voice_ogg, format="ogg").export(
         voice_mp3_path, format="mp3"
     )
-    return voice_mp3_path
+
+    with open(voice_mp3_path, "rb") as audio_file:
+        transcript = await client.audio.transcriptions.create(model="whisper-1", file=audio_file, response_format="text")
+    os.remove(voice_mp3_path)
+
+    assistant = await client.beta.assistants.create(
+        tools=[{"type": "code_interpreter"}],
+        model="gpt-4o"
+    )
+    thread = await client.beta.threads.create()
+    message = await client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=transcript
+    )
+    run = await client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
+    run_status = await client.beta.threads.runs.retrieve(thread_id=thread.id,
+                                                   run_id=run.id)
+    
+    while True:
+        run_status = await client.beta.threads.runs.retrieve(thread_id=thread.id, 
+                                                       run_id=run.id)
+        if run_status.status == "completed":
+            break
+        elif run_status.status == "failed":
+            print("Run failed:", run_status.last_error)
+            break
+        time.sleep(2)
+    
+    messages = await client.beta.threads.messages.list(
+        thread_id=thread.id
+    )
+
+    for message in reversed(messages.data): 
+        for content in message.content:
+            if content.type == "text":
+                text = content.text.value 
+                
+    speech_file_path = f"voice_files/speech-{voice.file_unique_id}.mp3"
+    response = await client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=text
+    )
+    
+    response.stream_to_file(speech_file_path)
+    speech_ogg_file = f"voice_files/speech-{voice.file_unique_id}.ogg"
+    AudioSegment.from_file(speech_file_path, format="mp3").export(
+        speech_ogg_file, format="ogg"
+    )
+    os.remove(speech_file_path)
+
+    return speech_ogg_file
 
 
 @dp.message(CommandStart())
 async def start(message: types.Message) -> None:
-    await message.answer('Привет! Это ChatGPT который отвечает на ваши голосовые команды.')
+    await message.answer("Привет! Это ChatGPT который отвечает на ваши голосовые команды.")
 
 
-@dp.message(F.content_type == 'voice')
+@dp.message(F.content_type == "voice")
 async def precess_voice_message(message: types.Message, bot: Bot) -> None:
     voice_path = await save_voice(bot, message.voice)
-    transcripted_voice_text = audio_to_text(voice_path)
 
-    if transcripted_voice_text:
-        voice = types.FSInputFile(transcripted_voice_text)
+    if voice_path:
+        voice = types.FSInputFile(voice_path)
         await bot.send_voice(message.chat.id, voice)
+        os.remove(f"voice_files/speech-{message.voice.file_unique_id}.ogg")
 
 
 async def main() -> None:
     await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
-    
